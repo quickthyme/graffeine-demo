487773002bf59c2f47770386a973b1d12a54bb22
Quickthyme <quickthyme@mac.com>
2020-01-10 01:43:33 -0600

    updated submodule
    Signed-off-by: Quickthyme <quickthyme@mac.com>

--- Sources/Graffeine-Demo/Main/RedGreenLines/RedGreenLinesCell.swift
+++ Sources/Graffeine-Demo/Main/RedGreenLines/RedGreenLinesCell.swift
@@ -9,11 +9,11 @@ class RedGreenLinesCell: UITableViewCell, DataAppliable {
 
     func applyData() {
         graffeineView.layer(id: LayerID.redLine)?.apply {
-            $0.data = GraffeineLayer.Data(valueMax: 50, values: [4, 11, 28, 22, 29, 31])
+            $0.data = GraffeineLayer.Data(valueMax: 50, values: [4, 11, 28, 22, 29, 31, nil])
         }
 
         graffeineView.layer(id: LayerID.greenLine)?.apply {
-            $0.data = GraffeineLayer.Data(valueMax: 50, values: [13, 28, 33, 44, 9, 18])
+            $0.data = GraffeineLayer.Data(valueMax: 50, values: [13, 28, 33, 44, 9, 18, nil])
         }
     }
 }
