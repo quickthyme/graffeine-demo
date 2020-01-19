4af697cccfead4fd7ec846d88f210cb647e45843
Quickthyme <quickthyme@mac.com>
2020-01-19 04:17:49 -0600

    lines demo trace animation
    Signed-off-by: Quickthyme <quickthyme@mac.com>

--- Sources/Graffeine-Demo/Main/RedGreenLines/RedGreenLinesCell.swift
+++ Sources/Graffeine-Demo/Main/RedGreenLines/RedGreenLinesCell.swift
@@ -4,24 +4,40 @@ import Graffeine
 class RedGreenLinesCell: UITableViewCell, DataAppliable {
 
     typealias LayerID = RedGreenLinesConfig.ID
-    
+
     @IBOutlet weak var graffeineView: GraffeineView!
 
+    @IBAction func buttonAction(_ sender: AnyObject?) {
+        applyData(animated: true)
+    }
+
     func applyData() {
-        graffeineView.layer(id: LayerID.redLine)?.apply {
-            $0.data = GraffeineData(valueMax: 50, values: [4, 11, 28, 22, 29, 31, nil])
-        }
+        applyData(animated: false)
+    }
 
-        graffeineView.layer(id: LayerID.redLineProj)?.apply {
-            $0.data = GraffeineData(valueMax: 50, values: [nil, nil, nil, nil, nil, 31, 33])
-        }
+    func applyData(animated: Bool) {
+        let lineAnimator = (animated)
+            ? GraffeineDataAnimators.Line.Trace(delay: 0, duration: 1.8, timing: .easeIn)
+            : nil
 
-        graffeineView.layer(id: LayerID.greenLine)?.apply {
-            $0.data = GraffeineData(valueMax: 50, values: [13, 28, 33, 44, 8, 16, nil])
-        }
+        let projectionLineAnimator = (animated)
+            ? GraffeineDataAnimators.Line.Trace(delay: 1.8, duration: 0.2, timing: .linear)
+            : nil
 
-        graffeineView.layer(id: LayerID.greenLineProj)?.apply {
-            $0.data = GraffeineData(valueMax: 50, values: [nil, nil, nil, nil, nil, 16, 24])
-        }
+        graffeineView.layer(id: LayerID.redLine)?
+            .setData(GraffeineData(valueMax: 50, values: [4, 11, 28, 22, 29, 31, nil]),
+                 animator: lineAnimator)
+
+        graffeineView.layer(id: LayerID.redLineProj)?
+            .setData(GraffeineData(valueMax: 50, values: [nil, nil, nil, nil, nil, 31, 33]),
+                     animator: projectionLineAnimator)
+
+        graffeineView.layer(id: LayerID.greenLine)?
+            .setData(GraffeineData(valueMax: 50, values: [13, 28, 33, 44, 8, 16, nil]),
+                 animator: lineAnimator)
+
+        graffeineView.layer(id: LayerID.greenLineProj)?
+            .setData(GraffeineData(valueMax: 50, values: [nil, nil, nil, nil, nil, 16, 24]),
+                     animator: projectionLineAnimator)
     }
 }
